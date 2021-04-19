735fad6a5e685aa50d02c6d739fd7009884a5c9e
SMarieMC <77701766+SMarieMC@users.noreply.github.com>
2021-04-18 18:31:48 -0700

    Add files via upload

---
+++ random code.py
@@ -0,0 +1,10 @@
+first_name = "Samantha"
+last_name = "McAdams"
+age = 35
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+hobbie = "Gaming"
+music = "Van Halen"
+print(f"My hobbies are {hobbie} and {music} is my favorite band.")
+x = "Python"
+y = "Coding Dojo"
+print(f"I just started using {x} from taking {y} and I enjoy learning from them.")
